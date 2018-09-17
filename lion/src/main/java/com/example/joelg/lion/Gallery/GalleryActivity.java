da5a5315b2ffda075e844e4876a1c01324e81497
Joel <joelgeri1@gmail.com>
2018-09-17 22:01:04 +1000

    Squashed commit of the following:
    commit 851f0ebfffc02aa876be8ddd61137b862b599956
    Merge: 8c5c65e 32aefd5
    Author: junzuke78 <joelgeri1@gmail.com>
    Date:   Thu Aug 23 14:56:12 2018 +1000
    
        AI-3.1.4 <Joel@DESKTOP-FK70J01 Merge branch 'master'
    
    commit 8c5c65ed8a228865da87c9bdf629c0c153dfc133
    Author: junzuke78 <joelgeri1@gmail.com>
    Date:   Thu Aug 23 14:56:12 2018 +1000
    
        AI-3.1.4 <Joel@DESKTOP-FK70J01 Update vcs.xml
    
    Signed-off-by: Joel <joelgeri1@gmail.com>

--- lion/src/main/java/com/example/joelg/lion/Gallery/GalleryActivity.java
+++ lion/src/main/java/com/example/joelg/lion/Gallery/GalleryActivity.java
@@ -1,5 +1,6 @@
 package com.example.joelg.lion.Gallery;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,8 +9,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
+import com.example.joelg.lion.Job.Lion;
 import com.example.joelg.lion.R;
+import com.example.joelg.lion.db.DaoSession;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -24,18 +30,27 @@ public class GalleryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        ImageView imgView;
+        long id;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        ImageView GalleryImg = findViewById(R.id.GalleryImg);
         RecyclerView RVG = findViewById(R.id.g_rv);
         LinearLayoutManager LLM = new LinearLayoutManager(this);
+        GridLayout GGG = new GridLayout(this);
+
         RVG.setLayoutManager(LLM);
         RVG.setHasFixedSize(true);
-        GalleryImage = new ArrayList<>();
 
-        GalleryAdapter galleryAdapter = new GalleryAdapter(GalleryImage);
-        RVG.setAdapter(galleryAdapter);
+        GGG.getRowCount();
 
+
+        imgView = new ImageView(this);
+        DaoSession daoSession = (( Lion ) imgView.getContext().getApplicationContext()).getDaoSession();
+        String tempImg = daoSession.toString();
+        Uri imgUri = Uri.parse(tempImg);
+        Picasso.get().load(imgUri).centerCrop().into(GalleryImg);
         Log.d("DEBUG_LION", "Job manager activated");
 
 
@@ -55,13 +70,11 @@ public class GalleryActivity extends AppCompatActivity {
         Log.d("DEBUG_LION", "Item selected :" + item.getItemId());
         switch (item.getItemId()) {
 
-            //Job_mgr_list.add("Job");
-            //if you want to start an activity
 
             default:
                 return super.onOptionsItemSelected(item);
         }
-        //private void OpenJob (View view)
+
     }
 
 }
